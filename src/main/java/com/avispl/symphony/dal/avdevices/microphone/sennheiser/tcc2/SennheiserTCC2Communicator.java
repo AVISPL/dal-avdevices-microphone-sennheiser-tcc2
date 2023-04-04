f3252d1733f9d3b0264352edd3960d871d796e1c
ThanhD-AVISPL <thanh.do@avispl.com>
2023-04-04 15:04:05 +0700

    SYAL-1250 update github action

--- src/main/java/com/avispl/symphony/dal/avdevices/microphone/sennheiser/tcc2/SennheiserTCC2Communicator.java
+++ src/main/java/com/avispl/symphony/dal/avdevices/microphone/sennheiser/tcc2/SennheiserTCC2Communicator.java
@@ -17,7 +17,6 @@ import java.util.List;
 import java.util.Map;
 import java.util.Set;
 import java.util.concurrent.CopyOnWriteArrayList;
-import java.util.concurrent.ExecutionException;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import java.util.concurrent.Future;
