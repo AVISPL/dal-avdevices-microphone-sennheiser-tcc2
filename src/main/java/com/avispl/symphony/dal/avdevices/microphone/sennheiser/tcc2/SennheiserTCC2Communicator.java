81463b6f4412bd057b8c1296f1ef19e57e4e2086
maksym.rossiitsev <maksym.rossiitsev@avispl.com>
2024-12-10 15:14:59 +0200

    SYAL-1949 Team Connect Sennheiser Mics Displaying Incorrect Link Status in Symphony When Adapter is Applied

--- src/main/java/com/avispl/symphony/dal/avdevices/microphone/sennheiser/tcc2/SennheiserTCC2Communicator.java
+++ src/main/java/com/avispl/symphony/dal/avdevices/microphone/sennheiser/tcc2/SennheiserTCC2Communicator.java
@@ -18,7 +18,6 @@ import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import java.util.concurrent.Future;
 import java.util.concurrent.locks.ReentrantLock;
-import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 
 import org.springframework.util.CollectionUtils;
